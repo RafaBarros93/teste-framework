d0096f7e9df259eda3c86fd7bf8cba008bc393de
rafaeldevfina <rafael@finafacil.com>
2022-02-18 12:59:19 -0300

    Hotfixes: Changes function isPrime().

--- src/domain/helpers/is-prime.js
+++ src/domain/helpers/is-prime.js
@@ -1,10 +1,12 @@
-module.exports = (num) => {
-  for (let i = 2; i < num; i++)
-    if (num % i === 0) {
-      return false
-    }
+module.exports = {
+  isPrime(num) {
+    for (let i = 2; i < num; i++)
+      if (num % i === 0) {
+        return false
+      }
 
-  let prime = num >= 1
+    let prime = num >= 1
 
-  return prime
+    return prime
+  }
 }
